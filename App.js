cb7f82b10e2e362deb403984f1ab03194fce2bfa
Otso Sallmen <127836033+Otsall@users.noreply.github.com>
2024-02-06 00:04:52 +0200

    typo korjaus

--- App.js
+++ App.js
@@ -1,33 +1,25 @@
-import React, { useState, useEffect} from "react";
-import { StyleSheet, Text, FlatList, SafeAreaView } from "react-native"
-import {DATA} from "./Data"
-import Row from "./Row"
-import Search from "./components/Search"
-
-const executeSearch = (search) => {
-  const searchArray = DATA.filter((item) => 
-    item.lastname.startsWith(search)) 
-    setItems(searchArray)
-}
+import React, { useState, useEffect } from "react";
+import { StyleSheet, FlatList, SafeAreaView } from "react-native";
+import { DATA } from "./Data";
+import Row from "./Row";
+import Search from "./components/Search";
 
 export default function App() {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState();
   useEffect(() => {
     setItems(DATA);
-  }, [])
+  }, []);
+  const executeSearch = (search) => {
+    const searchArray = DATA.filter((item) => item.lastname.startsWith(search));
+    setItems(searchArray);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Search executeSearch={executeSearch} />
-      <FlatList
-        data={items}
-        renderItem={({item}) => (
-        <Row person={item} />
-        )}
-      ></FlatList>
+      <FlatList data={items} renderItem={({ item }) => <Row person={item} />} />
     </SafeAreaView>
-  )
+  );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -36,4 +28,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-})
+});
